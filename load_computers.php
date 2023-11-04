468376e4947c375a0824ea49a9b414fc4b02aafd
leoesteban2 <leo01101100@gmail.com>
2023-11-04 17:51:24 -0600

    cambie nombre de laptop

--- load_computers.php
+++ load_computers.php
@@ -13,7 +13,7 @@ $computers = [
     "price" => "Q4000"
   ],
   [
-    "name" => "HP Computadora portátil",
+    "name" => "Acer Aspire 5 A515-56-347N",
     "description" => "Pantalla HD de 15.6\", procesador Intel Core i3-1115G4, 32 GB de RAM, SSD de 1 TB, Wi-Fi, lector de tarjetas SD, HDMI, cámara web, Windows 11 Home, azul abeto, accesorios KKE",
     "price" => "Q4000"
   ]
